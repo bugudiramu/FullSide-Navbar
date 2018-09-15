fd52d4f7dbef33dc7348eb158fde1df7216f5b15
ramubugudi <ramubugudi4@gmail.com>
2018-09-14 23:25:28 -0700

    Third commit

--- main.js
+++ main.js
@@ -4,15 +4,18 @@ const closeBtn = document.getElementById('closeBtn');
 const transparent = document.getElementById('transparent');
 const container = document.getElementById('container');
 const main = document.getElementById('main');
-
-
+// const dropdown = document.getElementById('dropdown');
+// const dropdownBtn = document.getElementById('dropdownBtn');
 
 openBtn.addEventListener('click', () => {
     nav.style.width = '100%';
-
-
 })
 closeBtn.addEventListener('click', () => {
     nav.style.width = '0px';
-
 })
+// dropdownBtn.addEventListener('click', () => {
+//     dropdown.style.display = 'none'
+// })
+// dropdownBtn.addEventListener('click', () => {
+//     dropdown.style.display = 'block'
+// })
